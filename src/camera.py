17bd85c4536527bcbf2555586ad4574c5f9b9c31
Kelian BAERT <kelian.baert@imt-atlantique.net>
2019-10-18 07:47:13 +0200

    refactored and added camera module and pictures

---
+++ src/camera.py
@@ -0,0 +1,36 @@
+import os
+import cv2 
+import random
+
+# Module-wide variable
+cam = {
+    "started": True
+};
+
+def turnCameraOn(onSuccess, onFailure):
+    global cam
+    cam["started"] = True
+    if onSuccess is not None:
+        onSuccess()
+    
+def turnCameraOff():
+    global cam
+    cam["started"] = False
+
+# For now, simply returns a random image from a folder    
+def takePicture():
+    if not cam["started"]:
+        return None
+    
+    # Get all images from the pictures directory
+    picturesDir = "../pictures"
+    pictureFiles = []
+    for item in os.listdir(picturesDir):
+        if os.path.isfile(os.path.join(picturesDir, item)) and (item.endswith(".png") or item.endswith(".jpg")):
+            pictureFiles.append(item)
+    # Select a random image
+    fileName = random.choice(pictureFiles)
+    # Read the image
+    img = cv2.imread(os.path.join(picturesDir, fileName))  
+    
+    return img
